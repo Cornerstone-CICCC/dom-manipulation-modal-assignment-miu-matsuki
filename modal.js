655c4027fea7e21b6ae9753ef1f6bea0f2980762
miu-matsuki <miu.m.works@gmail.com>
2026-07-14 12:28:08 -0700

    complete

--- modal.js
+++ modal.js
@@ -1,9 +1,27 @@
 console.log("here");
 
 // select button element
+const button = document.getElementById("openModal");
+const modal = document.getElementById("modal");
 
 // add event listener on click to button
-// remove class invisible from div modal
+button.addEventListener("click",()=>{
 
-// add event listener on key press Escape
-// add class invisible to div modal
+  // remove class invisible from div modal
+  modal.classList.remove("invisible");
+  // modal.removeAttribute("invisible");
+
+  setTimeout(()=>{
+    modal.classList.add("invisible");
+  }, 5000);
+
+})
+
+  // add event listener on key press Escape
+  document.addEventListener("keyup", (event)=>{
+    if(event.key === "Escape"){      
+      // add class invisible to div modal
+      modal.classList.add("invisible");
+    }
+  })
+
